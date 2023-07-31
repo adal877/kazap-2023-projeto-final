f50ddd3694c932c96e3486d8919f5bf8b8ec775f
adal <adalberto.luiz.santos.junior@gmail.com>
2023-07-30 21:24:32 -0300

    Application of the dotenv usage and setup into the app.rb file and project's environment

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
 require 'sequel'
+require 'dotenv'
 
-Sequel.sqlite('./db/bank.db')
+DB_PATH = ENV['DB_LOCATION']
+DB_NAME = ENV['DB_NAME']
+
+Sequel.sqlite("#{DB_PATH}/#{DB_NAME}")
+
+require_relative 'models/account'
+require_relative 'models/address'
+require_relative 'models/bank'
+require_relative 'models/client'
+require_relative 'models/clients_telephone'
+require_relative 'models/telephone'
+require_relative 'models/transaction'
+require_relative 'models/transactions_type'
